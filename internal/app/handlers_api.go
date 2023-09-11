dea3ce8149e0253b5f93c358694e58218b2d926f
RyanTrue <zufirka@icloud.com>
2023-09-11 19:33:24 +0300

    fix error

--- internal/app/handlers_api.go
+++ internal/app/handlers_api.go
@@ -295,10 +295,13 @@ func DeleteURL(handler Handler, w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	handler.Logger.Sugar.Debugf("DeleteURL body: %s", string(body))
+	handler.Logger.Sugar.Debug("DeleteURL body: ", string(body))
+	var reqArr []string
 
-	reqArr := []string{}
 	if err := json.Unmarshal(body, &reqArr); err != nil {
+		handler.Logger.Sugar.Debug("DeleteURL json.Unmarshal err = ", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
 	handler.Logger.Sugar.Debug("DeleteURL res: ", reqArr)
